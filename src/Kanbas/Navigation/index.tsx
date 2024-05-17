e20738a2d751825f9f8758cc38946035471c8f51
Diva1318 <diva1318.1813@gmail.com>
2024-05-17 12:11:31 -0400

    a2

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -1,13 +1,52 @@
+import { AiOutlineDashboard, AiOutlineDesktop, AiOutlineFullscreenExit, AiOutlineHistory, AiOutlineLaptop, AiOutlineLogout, AiOutlineQuestion, AiOutlineQuestionCircle, AiOutlineSetting } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCalendarAltSolid, LiaCogSolid, LiaInboxSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 export default function KanbasNavigation() {
     return (
-      <ul id="wd-kanbas-navigation">
-        <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
-        <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
-        <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
-        <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
-        <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
-        <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
-      </ul>
+      <div id="wd-kanbas-navigation" className="list-group rounded-0">
+        <a id="wd-neu-link" target="_blank" 
+        href="https://www.northeastern.edu/"
+        className="list-group-item bg-black border-0">
+        <img src="/images/NEU.png" width="75px"/> </a>
+
+        <a id="wd-account-link" href="#/Kanbas/Account"
+        className="list-group-item text-white
+        bg-black text-center border-0">
+        <FaRegCircleUser className="fs-1 text text-white" /><br />
+        Account</a>
+        <a id="wd-dashboard-link" href="#/Kanbas/Dashboard" className="list-group-item text-center border-0
+                   bg-white text-danger">
+        <AiOutlineDashboard className="fs-1 text-danger" /><br />Dashboard</a>
+        <a id="wd-course-link" href="#/Kanbas/Courses"
+        className="list-group-item text-white
+        bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />Courses</a>
+
+        <a id="wd-calendar-link" href="#/Kanbas/Calendar" 
+        className="list-group-item text-white
+        bg-black text-center border-0">
+        <LiaCalendarAltSolid className="fs-1 text-danger" /><br />Calendar</a>
+        <a id="wd-inbox-link" href="#/Kanbas/Inbox" className="list-group-item text-white
+        bg-black text-center border-0">
+        <LiaInboxSolid className="fs-1 text-danger" /><br />Inbox</a>
+        <a id="wd-labs-link" href="#/History"
+        className="list-group-item text-white
+        bg-black text-center border-0">
+        <AiOutlineHistory className="fs-1 text-danger" /><br />History</a>
+        <a id="wd-labs-link" href="#/Studio"
+        className="list-group-item text-white
+        bg-black text-center border-0">
+        <AiOutlineDesktop className="fs-1 text-danger" /><br />Studio</a>
+        <a id="wd-labs-link" href="#/Commons"
+        className="list-group-item text-white
+        bg-black text-center border-0">
+        <AiOutlineLogout className="fs-1 text-danger" /><br />Commons</a>
+        <a id="wd-labs-link" href="#/Help"
+        className="list-group-item text-white
+        bg-black text-center border-0">
+        <AiOutlineQuestionCircle className="fs-1 text-danger" /><br />Help</a>
+      </div>
   );}
   
   
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,25 +1,26 @@
 import Dashboard from "./Dashboard";
+import "./Styles.css";
 import KanbasNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
 export default function Kanbas() {
   return (
-    <div id="wd-kanbas">
-      <table width="100%">
-        <tr>
-          <td valign="top">
+    <div id="wd-kanbas" className="h-100">
+      <div className="d-flex h-100">
+      <div className="d-none d-md-block bg-black">
             <KanbasNavigation />
-          </td>
-          <td valign="top">
+            </div>
+            <div className="flex-fill p-4">
+
           <Routes>
               <Route path="/" element={<Navigate to="Dashboard" />} />
               <Route path="Dashboard" element={<Dashboard />} />
               <Route path="Courses/:id/*" element={<Courses />} />
               <Route path="Courses" element={<Dashboard />} />
             </Routes>
-      </td>
-        </tr>
-      </table>
+            </div>
+  </div>
+
 
     </div>
 );}
